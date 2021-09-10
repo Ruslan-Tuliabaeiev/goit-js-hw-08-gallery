84a72dcd6918fe1b25dc1a6951f0a1cd8a93fece
руслан <82108672+Ruslan-Tuliabaeiev@users.noreply.github.com>
2021-09-10 22:42:34 +0300

    2

--- app.js
+++ app.js
@@ -65,3 +65,147 @@ const galleryItems = [
       description: 'Lighthouse Coast Sea',
     },
   ];
+
+
+
+
+
+
+
+//   Создание и рендер разметки по массиву данных galleryItems из app.js и предоставленному шаблону.
+
+
+const gallery = document.querySelector('.gallery');
+
+const masiv = galleryFoto(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', masiv);
+
+function galleryFoto(items) {
+return items.map(({preview, original, description }) => {
+return `
+  <li class="gallery__item">
+    <a 
+      class="gallery__link"
+      href="${original}"   
+    >
+      <img
+         class="gallery__image"
+        src="${preview}"      
+        data-source="${original}"     
+        alt="${description}"   
+      />
+    </a>
+  </li>
+  
+  `;
+}).join('');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// }
+// elements => {return galleryItems.map(({preview,original,description}) => {
+//   return  `
+//   <li class="gallery__item">
+//     <a 
+//       class="gallery__link"
+//       href="${original}"   
+//     >
+//       <img
+//          class="gallery__image"
+//         src="${preview}"      
+//         data-source="${original}"     
+//         alt="${description}"   
+//       />
+//     </a>
+//   </li>
+  
+//   `
+// }).join('');
+
+// }
+
+// console.log(masiv);
+
+
+// const gallery = document.querySelector('#gallery');
+  
+// const fotoGallery = elements => {return elements.map(({url,alt}) => {
+  
+//     return `<li><img src="${url}" alt="${alt}" width="450px" height="300px"></li>`
+
+//   }).join('')
+// }
+
+
+
+
+
+
+
+// const galleryList = document.querySelector();
+// console.log();
+
+
+// const gallery = document.querySelector(".gallery");
+
+
+
+// <li class="gallery__item">
+//   <a
+//     class="gallery__link"
+//     href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+//   >
+//     <img
+//       class="gallery__image"
+//       src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
+//       data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+//       alt="Tulips"
+//     />
+//   </a>
+// </li>
+
+
+
+
+
+
+
+
+
+//   Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
+
+//   Открытие модального окна по клику на элементе галереи.
+
+//   Подмена значения атрибута src элемента img.lightbox__image.
+
+//   Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
+
+//   Очистка значения атрибута src элемента img.lightbox__image. Это необходимо для того, 
+//   чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.
+
+
+// <li class="gallery__item">
+//   <a
+//     class="gallery__link"
+//     href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+//   >
+//     <img
+//       class="gallery__image"
+//       src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
+//       data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+//       alt="Tulips"
+//     />
+//   </a>
+// </li>
